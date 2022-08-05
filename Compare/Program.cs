1658541699c6766f51c7c3635330027310ff1383
krossovka <krossovka@outlook.com>
2022-08-05 23:20:41 +0300

    Добавьте файлы проекта.

---
+++ Compare/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Нажмите Enter");
+while (Console.ReadLine() != "exit")
+{
+    Console.WriteLine("Введите первое число:");
+    int value3 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число:");
+    int value4 = Convert.ToInt32(Console.ReadLine());
+    if (value3 == value4)
+    {
+        Console.WriteLine("Числа равны");
+    }
+    if (value4 != value3)
+    {
+        Console.WriteLine("Числа не равны");
+    }
+    if (value3 > value4)
+    {
+        Console.WriteLine("Первое число больше");
+    }
+    if (value3 < value4)
+    {
+        Console.WriteLine("Второе число больше");
+
+    }
+    Console.WriteLine("Нажмите Enter");
+}
+
---
+++ Value Exchange/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 1;
+int b = 2;
+a = a + b;
+b = a - b;
+a = a - b;
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+Console.ReadKey();
