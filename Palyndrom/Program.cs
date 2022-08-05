6b2b28591c0e45c27a55cd404688d73ba7b867b4
krossovka <krossovka@outlook.com>
2022-08-06 00:42:05 +0300

    some changes

--- Palyndrom/Program.cs
+++ Palyndrom/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Введите значение");
+string s = Console.ReadLine();
+char[] str = s.ToCharArray();
+Array.Reverse(str);
+Console.Write(str);
+Console.ReadKey();
+
+
+if ()
+{
+
+}
+
