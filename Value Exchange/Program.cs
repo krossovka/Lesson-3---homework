d00507e08b238dcfffe7e2f38e39085724c41269
krossovka <krossovka@outlook.com>
2022-08-06 18:04:07 +0300

    apply recommended changes

--- Value Exchange/Program.cs
+++ Value Exchange/Program.cs
@@ -1,8 +1,12 @@
-﻿int a = 1;
-int b = 2;
+﻿Console.WriteLine("Введите первое число...");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число...");
+int b = Convert.ToInt32(Console.ReadLine()); ;
 a = a + b;
 b = a - b;
 a = a - b;
 Console.WriteLine("a = " + a);
 Console.WriteLine("b = " + b);
 Console.ReadKey();
+
+
